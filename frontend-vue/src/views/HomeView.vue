c2a0ea0e00b1059fbaf6db96c8c744760cd287d8
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-05-30 02:10:24 +0200

    show welcome message with username of user logged

--- frontend-vue/src/views/HomeView.vue
+++ frontend-vue/src/views/HomeView.vue
@@ -1,11 +1,26 @@
 <script>
-</script>
+import { useAuthStore } from '../stores/auth';
+export default{
 
+  data() {
+    return {
+      authStore : useAuthStore(),
+    };
+  },
+  methods: {
+  },
+  
+};
+</script>
 <template>
   <main>
     <div class="container">
       <div class="row text-center">
-        <h1>Gestisci la tua squadra con eFootball</h1>
+          <h1 v-if="authStore.user != null">
+            Benvenuto {{ authStore.user.username }}
+          </h1>
+        <h2>Gestisci la tua squadra con eFootball</h2>
+
       </div>
     </div>
   </main>
