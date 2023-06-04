36fe524e3fb23742b3bbd2ef8b010ae38954a600
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-06-04 20:54:36 +0200

    login form

--- frontend-angular/src/app/app.module.ts
+++ frontend-angular/src/app/app.module.ts
@@ -4,9 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtonModule } from 'primeng/button';
+import { HeaderComponent } from './component/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { RegisterComponent } from './pages/auth/register/register.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
